57876bc7fae44728163d3860195bf6d2586449da
Aman Kumar <kumaraman7056@hotmail.com>
2017-02-08 10:18:07 +0530

    This is the initialization dump for my project

---
+++ Demo-for-Git/Demo-for-Git/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Demo_for_Git.Startup))]
+namespace Demo_for_Git
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
